899a6fe884212acec59c58cc00004ed4fa8489c9
mjk8 <mjktt8@gmail.com>
2024-10-30 23:18:21 +0900

    Dialogue 사운드 추가

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -22,6 +22,7 @@ public class DialogueManager : MonoBehaviour
     private int nowEndDialogueId;
 
     public bool isDialogue = false;
+    private AudioSource _nextTextSound;
     
     private void Awake()
     {
@@ -31,6 +32,7 @@ public class DialogueManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             
             canvasDialogue = GetComponentInChildren<Canvas>();
+            _nextTextSound = GetComponent<AudioSource>();
             speakerText = canvasDialogue.GetComponentsInChildren<TextMeshProUGUI>()[0];
             dialogueText = canvasDialogue.GetComponentsInChildren<TextMeshProUGUI>()[1];
             canvasDialogue.enabled = false;
@@ -57,6 +59,7 @@ public class DialogueManager : MonoBehaviour
 
     public void ShowNextDialogue()
     {
+        _nextTextSound.Play();
         if (nowDialogueId <= nowEndDialogueId)
         {
             speakerText.text = dialogues.dialogues[nowDialogueId].speaker;
